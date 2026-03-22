ab0e2d97f83f2b41317180eb7cc9c963272cc8b9
kKrishGupta <kg20060220@gmail.com>
2026-03-23 02:12:41 +0530

    fix interview report model path

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,9 +7,12 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin :"http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://resume-maker-c6ko.vercel.app"
+  ],
   credentials: true
-}))
+}));
 // require all the routes here
 const authRouter = require('./routes/auth.routes');
 const interviewRouter = require("./routes/interview.routes");
