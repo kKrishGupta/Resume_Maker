7f5e3d26f0e75ca003275a967816258c7457e37e
kKrishGupta <kg20060220@gmail.com>
2026-03-23 09:54:36 +0530

    fixes routes and api calls

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
-  withCredentials: true
-});
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
+})
 
 export async function register({ username, email, password }) {
 try {
